d5c76c9afc55f237be955e3f7f5dc2fde029bd34
platonic <magicscript@163.com>
2019-06-20 17:32:45 +0800

    I just want to say that‘s my all day progressing

--- src/component/tabbar/HomeContainer.vue
+++ src/component/tabbar/HomeContainer.vue
@@ -1,13 +1,109 @@
 <template>
-    <div>
-        <h3>this is the HomePage</h3>
-    </div>
+  <div id="body">
+    <mt-swipe :auto="2000">
+      <mt-swipe-item v-for="item in PhotoList" :key="item.id">
+        <img :src="item.img" alt>
+      </mt-swipe-item>
+    </mt-swipe>
+    <!-- 栅格图标  li+span+div -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt>
+          <div class="mui-media-body">Home</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt>
+          <div class="mui-media-body">Email</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt>
+          <div class="mui-media-body">Chat</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu4.png" alt>
+          <div class="mui-media-body">location</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt>
+          <div class="mui-media-body">Search</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu6.png" alt>
+          <div class="mui-media-body">Phone</div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import { Toast } from "mint-ui";
 export default {
-    
-}
+  data() {
+    return {
+      name: "liudaxia",
+      PhotoList: []
+    };
+  },
+  created() {
+    this.getPicture();
+  },
+  methods: {
+    getPicture() {
+      this.$http
+        .get("http://www.liulongbin.top:3005/api/getlunbo")
+        .then(result => {
+          if (result.body.status == 0) {
+            Toast("加载轮播图成功！");
+            this.PhotoList = result.body.message;
+          } else {
+            console.log(result);
+            Toast("加载轮播图失败！");
+          }
+        });
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-
+#body {
+  .mint-swipe {
+    height: 180px;
+    .mint-swipe-item {
+      &:nth-child(1) {
+        background: lightblue;
+      }
+      &:nth-child(2) {
+        background: cyan;
+      }
+      &:nth-child(3) {
+        background: lightcoral;
+      }
+      img {
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+  .mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell {
+      border: 0;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import VueRouter from "vue-router"
 Vue.use(VueRouter)
 
 import router from "./router.js"
-import {Header} from "mint-ui"
+import {Header,Swipe,SwipeItem} from "mint-ui"
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
+import VueResource from "vue-resource"
+Vue.use(VueResource)
 import "./lib/css/mui.min.css"
 import "./lib/css/icons-extra.css"
 
